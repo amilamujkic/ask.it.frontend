35ecfaef738c814aede2864379b4dc629eef2f22
amilamjk <amilamjk@gmail.com>
2020-04-26 10:50:35 +0200

    fourth day

--- src/components/ask.js
+++ src/components/ask.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
---
+++ src/redux/types.js
@@ -0,0 +1,22 @@
+// User reducer types
+export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
+export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
+export const SET_USER = 'SET_USER';
+export const LOADING_USER = 'LOADING_USER';
+export const MARK_NOTIFICATIONS_READ = 'MARK_NOTIFICATIONS_READ';
+
+// UI reducer types
+export const SET_ERRORS = 'SET_ERRORS';
+export const LOADING_UI = 'LOADING_UI';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const LOADING_DATA = 'LOADING_DATA';
+export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+
+// Data reducer types
+export const SET_ASKS = 'SET_ASKS';
+export const SET_ASK = 'SET_ASK';
+export const LIKE_ASK = 'LIKE_ASK';
+export const UNLIKE_ASK = 'UNLIKE_ASK';
+export const DELETE_ASK = 'DELETE_ASK';
+export const POST_ASK = 'POST_ASK';
+export const SUBMIT_COMMENT = 'SUBMIT_COMMENT';
